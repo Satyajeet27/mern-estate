1bd95d9c7e0104ab7b08916ea3744f9f338fc758
Satyajeet27 <satyajeet.hitk@gmail.com>
2024-02-06 21:01:21 +0530

    create search api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -61,3 +61,38 @@ export const getListing= async(req, res, next)=>{
         next(error)
     }
 }
+export const getListings= async(req, res, next)=>{
+    try {
+        const limit= parseInt(req.query.limit) || 9
+        const startIndex= parseInt(req.query.startIndex) || 0
+        let offer= req.query.offer
+        if(!offer){
+             offer= {$in:[false, true]}
+        }
+        let furnished= req.query.furnished
+        if(!furnished){
+            furnished= {$in:[false, true]}
+        }
+        let parking= req.query.parking
+        if(!parking){
+            parking= {$in:[false, true]}
+        }
+        let type= req.query.type
+        if(type===undefined || type==="all"){
+            type= {$in:['sale', 'rent']}
+        }
+        const searchTerm= req.query.searchTerm || ""
+        const sort = req.query.sort || "createdAt"
+        const order= req.query.order || "desc"
+        const listings= await Listing.find({
+            name:{ $regex:searchTerm, $options:'i'},
+            offer,
+            furnished,
+            parking,
+            type
+        }).sort({[sort]:order}).limit(limit).skip(startIndex)
+        return res.status(200).send({success:true, listings})
+    } catch (error) {
+        next(error)
+    }
+}
