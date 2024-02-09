e8cff6ffef2c95cc77b9e743f296ba2cb17f1467
Satyajeet27 <satyajeet.hitk@gmail.com>
2024-02-10 00:34:32 +0530

    updated the header ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,25 +18,25 @@ const Header = () => {
   useEffect(()=>{
     const urlParams= new URLSearchParams(location.search)
     const searchTermFromUrl= urlParams.get('searchTerm')
-    console.log(searchTermFromUrl)
+    // console.log(searchTermFromUrl)
     if(searchTermFromUrl){
       return setSearchTerm(searchTermFromUrl)
     }
   },[location.search])
   return (
     <header className="bg-slate-200 shadow-md " >
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex flex-wrap justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Satya</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form onSubmit={handleSubmit} className="border max-sm:absolute max-sm:top-16 max-sm:w-80 bg-slate-100 p-2 rounded-lg flex justify-between items-center">
+        <form onSubmit={handleSubmit} className="max-sm:order-3 max-sm:mt-3 max-sm:w-full border bg-slate-100 p-1 sm:p-2 rounded-lg flex justify-between items-center">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none sm:w-64"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
             value={searchTerm}
             onChange={e=>setSearchTerm(e.target.value)}
           />
